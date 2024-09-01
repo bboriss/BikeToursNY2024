d60db3fafeaad737d6144a36ad177c920ac80ff5
bboriss <borisstevanovic0810@gmail.com>
2024-09-01 20:25:35 +0200

    build v3

--- components/Layout/ToursLayout.tsx
+++ components/Layout/ToursLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import styles from '../Layout/ToursLayout.module.scss';
+import styles from './ToursLayout.module.scss';
 
 interface ToursLayoutProps {
   children: React.ReactNode;
